de08302d2840cbfd66549c198f13c1d1cadce1ec
Ravi-dhanani <117377500+Ravi-dhanani@users.noreply.github.com>
2024-05-03 18:22:59 +0530

    produtc file delete

--- src/pages/component/detail/ProductSlider.tsx
+++
@@ -1,7 +0,0 @@
-import React, { useEffect, useState } from "react";
-import { Autoplay, Navigation, Pagination } from "swiper/modules";
-import { Swiper, SwiperSlide } from "swiper/react";
-
-export default function ProductSlider(props: any) {
-  return <div></div>;
-}
